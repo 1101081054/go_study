7dd2c35045087f7b5f0393fc47983041bae113f9
long <zuolong@songshuai.com.cn>
2019-08-16 16:28:06 +0800

    学习更新

--- study/nonrepeatingsubstr/nonrepeating.go
+++ study/nonrepeatingsubstr/nonrepeating.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 )
 
+var lastOccurred = make([]int, 0xffff)
+
 /**
  * 最长不重复子串
  */
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccurred := make(map[rune]int)
+	//lastOccurred := make(map[rune]int)
+
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
+
 	start := 0;
 	maxLength := 0
 
 	for i, ch := range []rune(s) {
-		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
 			start = lastI + 1
 		}
 		if i - start + 1 > maxLength {
